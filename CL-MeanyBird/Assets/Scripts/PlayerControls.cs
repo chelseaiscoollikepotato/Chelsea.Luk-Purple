97d09728ff191fc89ad86158e6e56ee3265335cd
Chelsea <chelsealuk421@outlook.com>
2022-07-21 18:26:11 -0400

    Step 11

---
+++ CL-MeanyBird/Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    //Game manager object
+    [Header("Game Controller Object for controlling the game")]
+    public GameController gameController;
+    [Header("Default Velocity")]
+    public float velocity = 5;
+    //Physics for the bird
+    private Rigidbody2D rb;
+    //height of the bird object on the y axis
+    private float objectHeight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Game Controller component
+        gameController = GetComponent<GameController>();
+        //Speed for the game is at a playing state
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
