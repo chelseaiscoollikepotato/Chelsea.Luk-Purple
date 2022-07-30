ef813e216bc53df4b637aa69eef0d39cb29d3249
Chelsea <chelsealuk421@outlook.com>
2022-07-30 13:51:52 -0400

    Step 16

---
+++ CL-SketchHead/Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerControls : MonoBehaviour
+{
+    //Rigidbody2D object that is stored
+    [Header("Rigidbody")]
+    public Rigidbody2D rb;
+    [Header("Default Down Speed")]
+    //Downward speed of the object
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
