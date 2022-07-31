2af7f056c5b0f8750802b8e41ba4b3bd0c6c175a
Chelsea <chelsealuk421@outlook.com>
2022-07-31 15:02:26 -0400

    Step 26

--- CL-SketchHead/Assets/Scripts/GameController.cs
+++ CL-SketchHead/Assets/Scripts/GameController.cs
@@ -5,15 +5,34 @@ using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
+    //Platform gameobject
+    [Header("Platform Object")]
+    public GameObject platform;
+    //Default position for platform
+    float pos = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        //Interger i equals 1000
+        for (int i = 0; i < 1000; i++)
+        {
+            //Execute SpawnPlatforms
+            SpawnPlatforms();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
 
+    }
+
+    //Spawn platforms function
+    void SpawnPlatforms()
+    {
+        //Spawn platforms randomly on the x axis and places them on the y axis every 2.5
+        Instantiate(platform, new Vector3(Random.value * 10 - 5f, pos, 0f), Quaternion.identity);
+        pos += 2.5f;
     }
 }
--- CL-SketchHead/Assets/Scripts/PlayerControls.cs
+++ CL-SketchHead/Assets/Scripts/PlayerControls.cs
@@ -10,15 +10,60 @@ public class PlayerControls : MonoBehaviour
     public Rigidbody2D rb;
     [Header("Default Down Speed")]
     //Downward speed of the object
+    public float downSpeed = 20f;
+    [Header("Default movement Speed")]
+    //Movement speed of the object
+    public float movementSpeed = 10f;
+    [Header("Default Directional Movement Speed")]
+    //Movement direction of the object
+    public float movement = 0f;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        //Variable equals to component Rigidbody2D
+        rb = GetComponent<Rigidbody2D>();    
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Movement equals Horizontal movement
+        //Multiplied by the movement speed
+        movement = Input.GetAxis("Horizontal") * movementSpeed;
+        //If direction on x axis is less than 0
+        if (movement < 0)
+        {
+            //Object faces to the left
+            this.GetComponent<SpriteRenderer>().flipX = false;
+        }
+        //If direction ox x axis is greater than 0
+        else
+        {
+            //Object faces to the right
+            this.GetComponent<SpriteRenderer>().flipX = true;
+        }
+    }
 
+    //FixedUpdate called every fixed rate frame
+    void FixedUpdate()
+    {
+        //Vector2 which is (x,y) velocity
+        //equals to the velocity of the Rigidbody2D
+        Vector2 velocity = rb.velocity;
+        //Velocity of the x axis equals to
+        //The direction movement on the x axis
+        //Of the character
+        velocity.x = movement;
+        //Rigidbody2D velocity equals to
+        //velocity of the object
+        rb.velocity = velocity; 
+    }
+
+    //Collision function
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //Velocity with the DownSpeed
+        rb.velocity = new Vector3(rb.velocity.x, downSpeed, 0);
     }
 }
