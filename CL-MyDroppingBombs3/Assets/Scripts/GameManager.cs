f6a27f30fb36155c88849869e32171ec731c803a
Chelsea <chelsealuk421@outlook.com>
2022-09-25 15:31:05 -0400

    Finished My Dropping Bombs 3

--- CL-MyDroppingBombs3/Assets/Scripts/GameManager.cs
+++ CL-MyDroppingBombs3/Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -10,6 +11,11 @@ public class GameManager : MonoBehaviour
     public GameObject playerPrefab;
     private GameObject player;
     private bool gameStarted = false;
+    public GameObject splash;
+    public GameObject scoreSystem;
+    public Text scoreText;
+    public int pointsWorth = 1;
+    private int score;
 
 
     void Awake()
@@ -18,12 +24,14 @@ public class GameManager : MonoBehaviour
         spawner = GameObject.Find("Spawner").GetComponent<Spawner>();
         screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
         player = playerPrefab;
+        scoreText.enabled = false;
     }
     // Start is called before the first frame update
     void Start()
     {
         spawner.active = false;
         title.SetActive(true);
+        splash.SetActive(false);
     }
 
     // Update is called once per frame
@@ -36,17 +44,27 @@ public class GameManager : MonoBehaviour
                 ResetGame();
 
             }
+        } else
+        {
+            if (!player)
+            {
+                OnPlayerKilled();
+            }
         }
 
         var nextBomb = GameObject.FindGameObjectsWithTag("Bomb");
 
         foreach (GameObject bombObject in nextBomb)
         {
-            if (bombObject.transform.position.y < (-screenBounds.y) - 12)
+            if (!gameStarted)
             {
                 Destroy(bombObject);
+        } else if (bombObject.transform.position.y < (-screenBounds.y) && gameStarted)
+        {
+                scoreSystem.GetComponent<Score>().AddScore(pointsWorth);
+            Destroy(bombObject);
             }
-     
+
         }
     }
 
@@ -54,7 +72,20 @@ public class GameManager : MonoBehaviour
     {
         spawner.active = true;
         title.SetActive(false);
+        splash.SetActive(false);
         player = Instantiate(playerPrefab, new Vector3(0, 0, 0), playerPrefab.transform.rotation);
         gameStarted = true;
+
+        scoreText.enabled = true;
+        scoreSystem.GetComponent<Score>().score = 0;
+        scoreSystem.GetComponent<Score>().Start();
+    }
+
+    void OnPlayerKilled()
+    {
+        spawner.active = false;
+        gameStarted = false;
+
+        splash.SetActive(true);
     }
 }
