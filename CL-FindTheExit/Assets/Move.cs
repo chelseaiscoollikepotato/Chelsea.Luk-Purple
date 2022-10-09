58e326f80285ac8562ec83b5feade92e9f454835
Chelsea <chelsealuk421@outlook.com>
2022-10-09 15:31:07 -0400

    Finished Find The Exit PY

---
+++ CL-FindTheExit/Assets/Move.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public float speed = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = 1;
+        Vector3 destination = new Vector3(horizontal, 0, vertical);
+        transform.Translate(destination * speed * Time.deltaTime);
+    }
+}
