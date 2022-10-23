e6b52ec1025283057a232a1f823168608d17e3fc
Chelsea <chelsealuk421@outlook.com>
2022-10-23 15:43:07 -0400

    Finished Jungle Escape PY

--- CL-JungleEscapePY/Assets/Scripts/DetectFalsePlatforms.cs
+++ CL-JungleEscapePY/Assets/Scripts/DetectFalsePlatforms.cs
@@ -7,6 +7,14 @@ public class DetectFalsePlatforms : MonoBehaviour
 
     void Update()
     {
-
+        bool hit = Physics.Raycast(transform.position, transform.forward, 2f, 1 << 8);
+        Debug.DrawRay(transform.position, transform.forward * 2f, Color.blue);
+       if (hit == true){
+            Debug.LogWarning("Be Careful!");
+        }
+        else
+        {
+            Debug.LogWarning("All Clear!");
+        }
     }
 }
