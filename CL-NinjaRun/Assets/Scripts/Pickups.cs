1455e3c18f03db64ed2cc161d6ebbe4a9ecfc909
Chelsea <chelsealuk421@outlook.com>
2022-10-27 18:04:05 -0400

    Finished Ninja Run / Started Ninja Run PY

--- CL-NinjaRun/Assets/Scripts/Pickups.cs
+++ CL-NinjaRun/Assets/Scripts/Pickups.cs
@@ -5,16 +5,24 @@ using UnityEngine.UI;
 
 public class Pickups : MonoBehaviour
 {
-    
+    public int score;
     public Text scoreText;
+
+    public ParticleSystem Pickup;
 
     void Start()
     {
-       
+        Pickup.Stop();
     }
 
-    void Update()
+    void OnTriggerEnter(Collider other)
     {
-    
+        if (other.gameObject.CompareTag("coin")){
+            score++;
+            scoreText.text = score.ToString();
+            Destroy(other.gameObject);
+
+            Pickup.Play();
+        }
     }
 }
