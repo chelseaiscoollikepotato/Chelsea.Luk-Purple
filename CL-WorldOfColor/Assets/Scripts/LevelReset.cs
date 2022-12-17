6ee2be8a90f80354d7b541e6d0faab1de6e474df
Chelsea <chelsealuk421@outlook.com>
2022-12-17 13:08:21 -0500

    Finished World Of Color / World Of Color PY

---
+++ CL-WorldOfColor/Assets/Scripts/LevelReset.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelReset : MonoBehaviour
+{
+    public ParticleSystem explosion;
+    public ParticleSystem trail;
+
+    public GameObject player;
+    public GameObject GameOverScreen;
+    void Start()
+    {
+        explosion.Stop();
+        trail.Play();
+        GameOverScreen.SetActive(false);
+    }
+
+    void fixedUpdate()
+    {
+        explosion.transform.position = player.transform.position;
+    }
+    public void GameOver()
+    {
+        player.SetActive(false);
+        Invoke("Reload", 2);
+        explosion.Play();
+        trail.Stop();
+        GameOverScreen.SetActive(true);
+    }
+
+    void Reload()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
+
+
